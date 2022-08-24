a8cc1ad30921535db4c8b378be1d07d8ec148757
Pronepoet <ochiengphillip4@gmail.com>
2022-08-24 16:08:59 +0300

    still working on it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.querySelector('create-task-form').addEventListener('submit', (event)=>{
+    event.preventDefault();
+   handleToDo(event.target.new-task-description.value);
+  })
+  function handleToDo(todo){
+    let p = document.createElement('p');
+    p.textContent = todo
+    document.querySelector('tasks').appendChild(p)
+  }
 });
